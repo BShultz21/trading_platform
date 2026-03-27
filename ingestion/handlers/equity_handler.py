1c3209e3994821b97d5f5b012d215675742e41e2
Braden <braden.shultz21@gmail.com>
2026-03-26 21:08:07 -0400

    Added logic to allow for writing of parquet files

--- ingestion/handlers/equity_handler.py
+++ ingestion/handlers/equity_handler.py
@@ -1,5 +1,10 @@
+from fastparquet import ParquetFile, write
 from auth import api_auth
 from requests import get
+import pandas as pd
+import datetime as dt
+
+
 
 if __name__ == '__main__':
     Schwab = api_auth.SchwabAPICredentials()
@@ -10,4 +15,13 @@ if __name__ == '__main__':
     url = 'https://api.schwabapi.com/marketdata/v1/pricehistory?symbol=AAPL&periodType=year&period=1&frequencyType=daily&frequency=1'
 
     response = get(url, headers=headers)
-    print(response.text)
+    data = {"timestamp" : [str(dt.datetime.now()).encode("UTF-8")],
+            "symbol": ["AAPL".encode("UTF-8")],
+            "asset_type": ["equities".encode("UTF-8")],
+            "raw_json": [response.text.encode("UTF-8")]}
+
+    df = pd.DataFrame(data=data)
+    write("languages.parquet", df, append=True)
+    pf = ParquetFile("languages.parquet")
+    df = pf.to_pandas()
+    print(df)
